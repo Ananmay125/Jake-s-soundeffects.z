23171385dc03f021dca0ca3a221fbdb664d875d5
Ananmay 125 <ananmay125@gmail.com>
2024-09-26 18:20:57 +0530

    Node and express

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App islistening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
